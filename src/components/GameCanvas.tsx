6bf360b6f62b0e38cbfd11e44c0923c0c15d6b08
Егор Двойняков <dvoinyakovegor00@gmail.com>
2025-08-23 16:01:57 +0300

    .

--- src/components/GameCanvas.tsx
+++ src/components/GameCanvas.tsx
@@ -752,7 +752,10 @@ export const GameCanvas = forwardRef<GameCanvasRef, GameCanvasProps>(
       participants: any[];
     }) => {
       console.log("Starting game with data:", gameData);
+        console.log('gamecanvas speedtime1:', speedUpTime)
+
       if (!appRef.current) return;
+      console.log("Starting game with data2:", gameData);
 
       seedRef.current = gameData.seed;
       randomRef.current = new DeterministicRandom(gameData.seed);
